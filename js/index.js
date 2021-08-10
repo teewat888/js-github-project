24f8d92d74603602d988e6f2261c82204714a530
teerawat prarom <teewat@yahoo.com>
2021-08-10 23:29:57 +1000

    add clear tags function to remove previous display tags

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const fetchService = (queryString, type = "user") => {
   const ulUser = document.getElementById("user-list");
   const ulRepo = document.getElementById("repos-list");
-  const p = document.createElement('p');
-  const container = document.getElementById('github-container');
+  const p = document.createElement("p");
+  const container = document.getElementById("github-container");
   const confObj = {
     method: "GET",
     headers: {
@@ -11,41 +11,55 @@ const fetchService = (queryString, type = "user") => {
     },
   };
   let li;
-  const clearUl = () => {
+  const clearTags = () => {
     while (ulUser.firstChild) {
-        ulUser.removeChild(ulUser.firstChild);
-      }
-  }
+      ulUser.removeChild(ulUser.firstChild);
+    }
+    while (ulRepo.firstChild) {
+      ulRepo.removeChild(ulRepo.firstChild);
+    }
+    const p1 = document.querySelector("p[name='user']");
+    if (p1 !== null) {
+      p1.remove();
+    }
+  };
+  
+
   const createUserList = (obj) => {
-    clearUl();
+    clearTags();
+
     for (const item of obj.items) {
       li = document.createElement("li");
-      li.setAttribute("data-id",item.id);
+      li.setAttribute("data-id", item.id);
       li.innerHTML = `
         <img src="${item.avatar_url}" width="25" height="25" />
         <b>${item.login}</b>`;
       ulUser.appendChild(li);
-      li.addEventListener('click', () => {
+      li.addEventListener("click", () => {
         fetchService(item.login, "repos");
-      })
+      });
     }
-  }
+  };
 
-  const createRepolist = (obj,queryString) => {
-      console.log(obj);
-      clearUl();
-      p.innerHTML = `<b>${queryString}</b>`;
-      container.insertBefore(p, container.firstElementChild.nextSibling);
-      for(const item of obj) {
-        li = document.createElement("li");
-        li.setAttribute("data-id",item.id);
-        li.innerHTML = `
+  const createRepolist = (obj) => {
+    console.log(obj);
+    clearTags();
+    
+    const p1 = document.querySelector("p[name='user']");
+    if (p1 !== null) {
+      p1.remove();
+    }
+    p.innerHTML = `<img src="${obj[0].owner.avatar_url}" width="25" height="25" /><b>${obj[0].owner.login}'s repos => </b>`;
+    p.setAttribute("name", "user");
+    container.insertBefore(p, container.firstElementChild);
+    for (const item of obj) {
+      li = document.createElement("li");
+      li.setAttribute("data-id", item.id);
+      li.innerHTML = `
             <b>${item.name}</b>`;
-        ulRepo.appendChild(li);
-      } 
-
-
-  }
+      ulRepo.appendChild(li);
+    }
+  };
 
   if (type === "user") {
     return fetch(
@@ -61,16 +75,17 @@ const fetchService = (queryString, type = "user") => {
         console.log(e);
       });
   } else {
-      return fetch(
-         `https://api.github.com/users/${queryString}/repos?per_page=100`,confObj)
-      .then( (resp) => resp.json()
-      )
-      .then( (obj) => {
-        createRepolist(obj,queryString);
+    return fetch(
+      `https://api.github.com/users/${queryString}/repos?per_page=100`,
+      confObj
+    )
+      .then((resp) => resp.json())
+      .then((obj) => {
+        createRepolist(obj);
       })
-      .catch( (e) => {
-          console.log(e);
-      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 };
 
