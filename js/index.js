b9834f8158387eb96fd314c28ea22876642b8f69
teerawat prarom <teewat@yahoo.com>
2021-08-10 23:10:20 +1000

    completed user list and repo list

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 const fetchService = (queryString, type = "user") => {
   const ulUser = document.getElementById("user-list");
   const ulRepo = document.getElementById("repos-list");
+  const p = document.createElement('p');
+  const container = document.getElementById('github-container');
+  const confObj = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.v3+json",
+    },
+  };
   let li;
   const clearUl = () => {
     while (ulUser.firstChild) {
@@ -8,36 +17,60 @@ const fetchService = (queryString, type = "user") => {
       }
   }
   const createUserList = (obj) => {
+    clearUl();
     for (const item of obj.items) {
       li = document.createElement("li");
+      li.setAttribute("data-id",item.id);
       li.innerHTML = `
         <img src="${item.avatar_url}" width="25" height="25" />
         <b>${item.login}</b>`;
       ulUser.appendChild(li);
+      li.addEventListener('click', () => {
+        fetchService(item.login, "repos");
+      })
     }
-  };
+  }
+
+  const createRepolist = (obj,queryString) => {
+      console.log(obj);
+      clearUl();
+      p.innerHTML = `<b>${queryString}</b>`;
+      container.insertBefore(p, container.firstElementChild.nextSibling);
+      for(const item of obj) {
+        li = document.createElement("li");
+        li.setAttribute("data-id",item.id);
+        li.innerHTML = `
+            <b>${item.name}</b>`;
+        ulRepo.appendChild(li);
+      } 
+
+
+  }
 
   if (type === "user") {
-    const confObj = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/vnd.github.v3+json",
-      },
-    };
     return fetch(
       `https://api.github.com/search/users?q=${queryString}`,
       confObj
     )
       .then((resp) => resp.json())
       .then((obj) => {
-        console.log("Obj: ", obj);
-        clearUl();
+        //console.log("Obj: ", obj);
         createUserList(obj);
       })
       .catch((e) => {
         console.log(e);
       });
+  } else {
+      return fetch(
+         `https://api.github.com/users/${queryString}/repos?per_page=100`,confObj)
+      .then( (resp) => resp.json()
+      )
+      .then( (obj) => {
+        createRepolist(obj,queryString);
+      })
+      .catch( (e) => {
+          console.log(e);
+      })
   }
 };
 
